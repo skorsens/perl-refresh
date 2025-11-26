20e4703810cfff31c2de05cf1ef080fdb6ef8e62
Sergey Korsensky <skorsens@gmail.com>
2025-11-26 12:45:27 +0200

    Try Cursor rules

--- lists_and_arrays.pl
+++ lists_and_arrays.pl
@@ -19,85 +19,85 @@ print '(1..4) == ', join(", ", (1..4)), "\n"; # Gives 1, 2, 3, 4
 print '(4..1) == ', join(", ", (4..1)), "\n"; 
 
 # List elements can be variables and those elements are evaluated only once
-$n1 = 1;
-$n2 = 2;
-@a1 = ($n1, $n2);
-print "\@a1 == ", join(", ", @a1), "\n"; # @a1 == (1, 2)
-$n1 += 3;
-print "\$n1 == $n1\n";
-print "\@a1 == ", join(", ", @a1), "\n"; # @a1 == (1, 2)
+$nVal1 = 1;
+$nVal2 = 2;
+@lArr1 = ($nVal1, $nVal2);
+print "\@lArr1 == ", join(", ", @lArr1), "\n"; # @lArr1 == (1, 2)
+$nVal1 += 3;
+print "\$nVal1 == $nVal1\n";
+print "\@lArr1 == ", join(", ", @lArr1), "\n"; # @lArr1 == (1, 2)
 
 # The qw (quoted words) operator
-@a2 = qw(one two three); # ("one", "two", "three")
-print "\@a2 == ", join(", ", @a2), "\n";
+@lArr2 = qw(one two three); # ("one", "two", "three")
+print "\@lArr2 == ", join(", ", @lArr2), "\n";
 
 # List assignments
-($n0, $n1) = (1, 2, 3); # $n0 == 1, $n1 == 2
-print "\$n0 == $n0, \$n1 == $n1\n";
-($n0, $n1, $n2) = (1, 2, ); # $n0 == 1, $n1 == 2, $n2 == undef
-print "\$n0 == $n0, \$n1 == $n1, \$n2 == $n2\n";
+($nVal0, $nVal1) = (1, 2, 3); # $nVal0 == 1, $nVal1 == 2
+print "\$nVal0 == $nVal0, \$nVal1 == $nVal1\n";
+($nVal0, $nVal1, $nVal2) = (1, 2, ); # $nVal0 == 1, $nVal1 == 2, $nVal2 == undef
+print "\$nVal0 == $nVal0, \$nVal1 == $nVal1, \$nVal2 == $nVal2\n";
 
 # The nested arrays are expanded to their members
-@a3 = (@a1, @a2, 7, 8, 9); # @a3 == 1, 2, one, two, three, 7, 8, 9
-print "\@a3 == ", join(", ", @a3), "\n";
+@lArr3 = (@lArr1, @lArr2, 7, 8, 9); # @lArr3 == 1, 2, one, two, three, 7, 8, 9
+print "\@lArr3 == ", join(", ", @lArr3), "\n";
 
 # Copy arrays
-@a4 = @a1;
-print "\@a1 == ", join(", ", @a1), "\n";
-$a4[0] = 43;
-print "\@a4 == ", join(", ", @a4), "\n";
+@lArr4 = @lArr1;
+print "\@lArr1 == ", join(", ", @lArr1), "\n";
+$lArr4[0] = 43;
+print "\@lArr4 == ", join(", ", @lArr4), "\n";
 
 # pop and push
-$e1 = pop(@a1); # $e1 == 2, @a1 == (1, )
-print "\$e1 == $e1, \@a1 == ", join(", ", @a1), "\n";
-push @a1, 2; # @a1 == (1, 2, )
-print "\@a1 == ", join(", ", @a1), "\n";
+$nElem1 = pop(@lArr1); # $nElem1 == 2, @lArr1 == (1, )
+print "\$nElem1 == $nElem1, \@lArr1 == ", join(", ", @lArr1), "\n";
+push @lArr1, 2; # @lArr1 == (1, 2, )
+print "\@lArr1 == ", join(", ", @lArr1), "\n";
 
 # shift and unshift: same as pop and push, but for the array front
 
 # ##############################################
 # Remove elements with the splice operator 
-@a5 = (1, 2, 3, 4, 5);
-@a51 = splice @a5, 2; # @a51 == (3, 4, 5, )
-print "\@a5 == ", join(", ", @a5), "\n";
-print "\@a51 == ", join(", ", @a51), "\n";
+@lArr5 = (1, 2, 3, 4, 5);
+@lArr51 = splice @lArr5, 2; # @lArr51 == (3, 4, 5, )
+print "\@lArr5 == ", join(", ", @lArr5), "\n";
+print "\@lArr51 == ", join(", ", @lArr51), "\n";
 
-@a5 = (1, 2, 3, 4, 5);
-@a51 = splice @a5, 2, 2; # @a51 == (3, 4, )
-print "\@a5 == ", join(", ", @a5), "\n";
-print "\@a51 == ", join(", ", @a51), "\n";
+@lArr5 = (1, 2, 3, 4, 5);
+@lArr51 = splice @lArr5, 2, 2; # @lArr51 == (3, 4, )
+print "\@lArr5 == ", join(", ", @lArr5), "\n";
+print "\@lArr51 == ", join(", ", @lArr51), "\n";
 
-@a5 = (1, 2, 3, 4, 5);
-@a51 = splice @a5, 2, 0, (6, 7, ); 
-print "\@a5 == ", join(", ", @a5), "\n"; # @a5 == (1, 2, 3, 6, 7, 4, 5, )
-print "\@a51 == ", join(", ", @a51), "\n"; # @a1 == ()
+@lArr5 = (1, 2, 3, 4, 5);
+@lArr51 = splice @lArr5, 2, 0, (6, 7, ); 
+print "\@lArr5 == ", join(", ", @lArr5), "\n"; # @lArr5 == (1, 2, 3, 6, 7, 4, 5, )
+print "\@lArr51 == ", join(", ", @lArr51), "\n"; # @lArr1 == ()
 
 # Array interpolation into a string
-print "\@a5 == @a5\n"; # @a5 == 1 2 6 7 3 4 5
+print "\@lArr5 == @lArr5\n"; # @lArr5 == 1 2 6 7 3 4 5
 
 # When a string is used in a numeric expression, Perl converts
 # the string to number until the first non-number element in the string.
-# Hence, $z == 4 - 1 == 3 and not 12 - 1 == 11
-$y = "4*3";
-$z = $y - 1;
-print "$y - 1; $z\n";
+# Hence, $nZ == 4 - 1 == 3 and not 12 - 1 == 11
+$sY = "4*3";
+$nZ = $sY - 1;
+print "$sY - 1; $nZ\n";
 
 # foreach statement
-foreach $e (@a5) {
-    print "\$e == $e, ";
-    # The control variable ($e) is the reference to the list elements
+foreach $nElem (@lArr5) {
+    print "\$nElem == $nElem, ";
+    # The control variable ($nElem) is the reference to the list elements
     # Changing it changes the list elements
-    # @a5 == 2 3 7 8 4 5 6 after the loop
-    $e += 1;
+    # @lArr5 == 2 3 7 8 4 5 6 after the loop
+    $nElem += 1;
 }
-print "\n\@a5 == @a5\n";
+print "\n\@lArr5 == @lArr5\n";
 
-# The value of control variable ($cv) remains the same after the foreach
-$cv = 1;
-foreach $cv (1,2,3) {
-    print "$cv, ";
+# The value of control variable ($nControlVar) remains the same after the foreach
+$nControlVar = 1;
+foreach $nControlVar (1,2,3) {
+    print "$nControlVar, ";
 }
-print "\$cv == $cv\n"; # $cv = 1
+print "\$nControlVar == $nControlVar\n"; # $nControlVar = 1
 
 # The favorite default var $_
 foreach (1..3) {
@@ -106,34 +106,34 @@ foreach (1..3) {
 }
 
 # The reverse operator
-@a7 = (1,2,3,);
-@a7 = reverse(@a7);
-print "\@a7 == @a7\n"; # 3 2 1
+@lArr7 = (1,2,3,);
+@lArr7 = reverse(@lArr7);
+print "\@lArr7 == @lArr7\n"; # 3 2 1
 
 # The sort operator by default sorts in Unicode postcodes order.
 # By default, the numbers are sorted as strings.
-@a8 = 97..102;
-@a8 = sort(@a8); # @a8 == 100 101 102 97 98 99
-print "\@a8 == @a8\n";
+@lArr8 = 97..102;
+@lArr8 = sort(@lArr8); # @lArr8 == 100 101 102 97 98 99
+print "\@lArr8 == @lArr8\n";
 
 # The each operator returns the index and the value
-@a9 = qw / e1 e2 e3 /;
-while (($i, $e) = each @a9){
-    print "\$i, \$e == $i, $e\n";
+@lArr9 = qw / e1 e2 e3 /;
+while (($nIndex, $sElem) = each @lArr9){
+    print "\$nIndex, \$sElem == $nIndex, $sElem\n";
 }
 
 # List producing expressions in the scalar context
-$s2 = reverse qw / one two three /;
-@s2 = reverse qw / one two three /;
-# $s2 == eerhtowteno; @s2 == three two one 
-print "\$s2 == $s2; \@s2 == @s2\n";
+$sReversed = reverse qw / one two three /;
+@lReversed = reverse qw / one two three /;
+# $sReversed == eerhtowteno; @lReversed == three two one 
+print "\$sReversed == $sReversed; \@lReversed == @lReversed\n";
 
-# `scalar` forces scalar context: @s2 == three two one; scalar @s2 == 3
-print "\@s2 == @s2; ", 'scalar @s2 == ', scalar @s2, "\n";
+# `scalar` forces scalar context: @lReversed == three two one; scalar @lReversed == 3
+print "\@lReversed == @lReversed; ", 'scalar @lReversed == ', scalar @lReversed, "\n";
 
 # `<STDIN>` in list context returns a list of the input strings until EOF - 
 # Ctrl-D on Linux, Ctrl-Z on Windows
 # print "Enter lines and press Ctrl-Z or D:\n";
-# chomp(@aInputs = <STDIN>);
-# print "\@aInputs == @aInputs\n"; # @aInputs == l1 l2 l3
+# chomp(@lInputs = <STDIN>);
+# print "\@lInputs == @lInputs\n"; # @lInputs == l1 l2 l3
 
